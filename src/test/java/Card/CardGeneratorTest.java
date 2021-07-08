0caf7f19b16922d4fca5b3a921b2246bb5b52309
Gguinea <kamil5176@gmail.com>
2021-07-09 00:07:05 +0200

    Removed unused import

--- src/test/java/Card/CardGeneratorTest.java
+++ src/test/java/Card/CardGeneratorTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CardGeneratorTest {
     @Test
